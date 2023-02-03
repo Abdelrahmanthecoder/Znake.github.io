601a4f60353930ffbbcca7a2c40f68dc94ba70e0
Abdelrahman B <99421749+Abdelrahmanthecoder@users.noreply.github.com>
2023-02-03 06:36:17 +0300

    snake.js updated

--- snake.js
+++ snake.js
@@ -7,6 +7,7 @@ var score = 0;
 
 var DeathAudio = new Audio("/DeathAudio.mp3");
 var AppleCrunchAudio = new Audio("/AppleCrunchAudio.mp3");
+var PopAudio = new Audio("/PopAudio.mp3");
 
 var snakeX = blockSize * 15;
 var snakeY = blockSize * 15;
@@ -88,21 +89,25 @@ function changeDirection(e) {
         velocityX = 0;
         velocityY = -1;
         console.log("ArrowUp")
+        PopAudio.play();
     }
     else if (e.code == "ArrowDown" && velocityY != -1) {
         velocityX = 0;
         velocityY = 1;
         console.log("ArrowDown")
+        PopAudio.play();
     }
     else if (e.code == "ArrowLeft" && velocityX != 1) {
         velocityX = -1;
         velocityY = 0;
         console.log("ArrowLeft")
+        PopAudio.play();
     }
     else if (e.code == "ArrowRight" && velocityX != -1) {
         velocityX = 1;
         velocityY = 0;
         console.log("ArrowRight")
+        PopAudio.play();
     }
     else if (e.code == "KeyW" && velocityY != 1) {
         velocityX = 0;
